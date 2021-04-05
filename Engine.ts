8abc0af758a8afcdf66af12b52a6b4067016059e
Arbaz Ajaz <arbaz5256@gmail.com>
2021-04-05 19:53:18 +0500

    feat: html fields to control engine configuration

--- Engine.ts
+++ Engine.ts
@@ -8,6 +8,8 @@ export default class Engine {
   public particles: Particle[];
   public timeStep = 0.005;
   public time = 0;
+  public shouldRenderCells = false;
+  public shouldRenderCellsAngle = false;
 
   constructor(
     public canvas: HTMLCanvasElement,
@@ -16,13 +18,53 @@ export default class Engine {
     public resolution = 0.05
   ) {
     this.ctx = canvas.getContext("2d");
-    console.log(noise.get(0, 0, 0))
+    console.log(noise.get(0, 0, 0));
+  }
+
+  renderCells() {
+    const { canvas, cellWidth, ctx, shouldRenderCellsAngle, noise } = this;
+    const cols = Math.floor(canvas.width / cellWidth);
+    const rows = Math.floor(canvas.height / cellWidth);
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        const x = j * cellWidth;
+        const y = i * cellWidth;
+        // ctx.beginPath();
+        // ctx.strokeStyle = "#ffffff1a";
+        // ctx.rect(x, y, cellWidth, cellWidth);
+        // ctx.stroke();
+        // ctx.closePath();
+        if (shouldRenderCellsAngle) {
+          const { resolution, time } = this;
+          const xoff = j * resolution;
+          const yoff = i * resolution;
+          const angle = noise.get(xoff, yoff, time);
+          const vector = new Vector(1, 0)
+            .setMag(cellWidth)
+            .setAngle(angle * Math.PI * 4);
+          ctx.beginPath();
+          ctx.fillStyle = "#ffffff2a";
+          ctx.arc(x, y, 1, 0, Math.PI * 2);
+          ctx.fill();
+          ctx.closePath();
+
+          ctx.beginPath();
+          ctx.strokeStyle = "#ffffff2a";
+          ctx.moveTo(x, y);
+          ctx.lineTo(x + vector.x, y + vector.y);
+          ctx.stroke();
+          ctx.closePath();
+        }
+      }
+    }
   }
 
   draw() {
     requestAnimationFrame(this.draw.bind(this));
 
     this.fillBackground("#0000001a");
+
+    if (this.shouldRenderCells) this.renderCells();
 
     this.particles.forEach((particle) => {
       const { cellWidth, resolution, noise, time } = this;
@@ -33,11 +75,11 @@ export default class Engine {
       const cellCol = x / cellWidth;
       const cellRow = y / cellWidth;
 
-      const noiseX = cellRow * resolution;
-      const noiseY = cellCol * resolution;
+      const noiseX = cellCol * resolution;
+      const noiseY = cellRow * resolution;
       const forceAngle = noise.get(noiseX, noiseY, time) * Math.PI;
 
-      const force = new Vector(1, 1).setMag(1).setAngle(forceAngle*4);
+      const force = new Vector(1, 1).setMag(1).setAngle(forceAngle * 4);
 
       particle.applyForce(force);
       particle.update();
